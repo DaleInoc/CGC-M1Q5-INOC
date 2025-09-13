3e3f73e20638ab18d7231c15cd5957c571219a08
DaleInoc <ddInoc@mcm.edu.ph>
2025-09-13 14:16:55 +0800

    Added 5 geometries and animations

--- main.js
+++ main.js
@@ -8,17 +8,62 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.setAnimationLoop( animate );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
+const cubeGeometry = new THREE.BoxGeometry( 3, 3, 3 );
+const cubeMaterial = new THREE.MeshBasicMaterial( { color: 0xeb4034 } );
+const cube = new THREE.Mesh( cubeGeometry, cubeMaterial);
+cube.position.set( 0, 5, -7);
 scene.add( cube );
 
+const coneGeometry = new THREE.ConeGeometry( 5, 5, 32 );
+const coneMaterial = new THREE.MeshBasicMaterial( {color: 0x8934eb} );
+const cone = new THREE.Mesh(coneGeometry, coneMaterial);
+cone.position.set( 0, 0, -7);
+scene.add( cone );
+
+const cylinderGeometry = new THREE.CylinderGeometry( 2.5, 2.5, 10, 32 );
+const cylinderMaterial = new THREE.MeshBasicMaterial( {color: 0xfc4eb4} );
+const cylinder = new THREE.Mesh(cylinderGeometry,cylinderMaterial);
+scene.add( cylinder );
+cylinder.position.set( -12, 0, -10);
+
+const sphereGeometry = new THREE.SphereGeometry( 3, 32, 16 );
+const sphereMaterial = new THREE.MeshBasicMaterial( { color: 0x057320 } );
+const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+scene.add( sphere );
+sphere.position.set( 12, -1, -10);
+
+const ringGeometry = new THREE.TorusGeometry(2,0.5,16,100);
+const ringMaterial = new THREE.MeshBasicMaterial( {color: 0x197564 } );
+const ring = new THREE.Mesh( ringGeometry, ringMaterial);
+scene.add( ring );
+ring.position.set(0,-8,-10);
+
+
 camera.position.z = 5;
+
+var cylinderTargetPosition = -15;
+var sphereTargetPosition = 10;
 
 function animate() {
 
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
+
+    cone.rotation.y += 0.01;
+
+    if(cylinder.position.z > cylinderTargetPosition) {
+        cylinder.position.z -= 0.05;
+    } else {
+        cylinder.position.z = -10;
+    }
+
+    if(sphere.position.y < sphereTargetPosition) {
+        sphere.position.y += 0.05;
+    } else {
+        sphere.position.y = -1;
+    }
+
+    ring.rotation.x += 0.01;
 
     renderer.render( scene, camera );
 
